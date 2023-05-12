776b91d4f795a69b641dabd59b14b98802644f9e
Sergeev36 <sergeev.36rus@gmail.com>
2023-05-12 20:42:10 +0300

    Configure webpack

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './index.scss'
+
+const App = () => {
+  return (
+    <div className='app'>
+      123
+    </div>
+  );
+};
+
+export default App;
---
+++ src/test.ts
@@ -0,0 +1,3 @@
+export function someFn (): any {
+  console.log('RANDOM')
+}
